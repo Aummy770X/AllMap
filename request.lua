eb6cc57b3557e7b274c902b1ae1bf72119857c52
Aummy770X <101350801+Aummy770X@users.noreply.github.com>
2023-09-29 16:32:23 +0700

    Update request.lua

--- request.lua
+++ request.lua
@@ -5,5 +5,5 @@ if game.PlaceId == 13621938427 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Aummy770X/Secret-HUB/main/Secret.lua"))()
 end
 if game.PlaceId == 13772394625 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Aummy770X/Secret-HUB/main/BladeBall.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aummy770X/AllMap/main/BladeBall.lua"))()
 end
