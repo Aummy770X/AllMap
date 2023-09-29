25c4fa157e388fbd0502f3be361bc1febba40d14
Aummy770X <101350801+Aummy770X@users.noreply.github.com>
2023-09-29 14:42:55 +0700

    Create request.lua

---
+++ request.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 14483937954 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Aummy770X/AllMap/main/ObbyBike.lua')))()
+elseif game.PlaceId == 13621938427 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aummy770X/Secret-HUB/main/Secret.lua"))()
+end
